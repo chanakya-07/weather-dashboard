71462f330caf758cfa77ed25a2f93eec14481a07
NikhilPalliCode <nikhilpalle1997@gmail.com>
2025-03-14 00:54:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# WeatherApp
+## Description
+This is a weather application built using Angular 19, designed to provide users weather information, in real time, for various locations worldwide.
+
+## Overview
+I implemented an API request to Weather API ([https://www.weatherapi.com/]) to show information about weather for a searched city.
+The user searches for a city, presses enter and it will appear informations about minimum and maximum temperature, humidity and wind.
+
+<img src="https://github.com/user-attachments/assets/46afc86c-945e-47cd-ac4d-8555310dc254" style="width:50%; height:50%;">
+<img src="https://github.com/user-attachments/assets/3821a843-7b43-41ac-bf91-f0cf25c3a3f6" style="width:50%; height:auto";>
+
+
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DecimalPipe } from '@angular/common';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.models';
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,FormsModule, DecimalPipe],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  weatherData!:any;
+  cityName: string = "Timisoara";
+
+  url = `http://api.weatherapi.com/v1/current.json`;
+  key = 'ebd317e8cbb14aa6ab6204524251801';
+  
+
+  constructor(private weatherService: WeatherService) {}
+
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName = "";
+}
+
+  onSubmit(){
+    this.getWeatherData(this.cityName);
+    this.cityName = "";
+  }
+
+  private getWeatherData(cityName: string){
+    this.weatherService.getWeatherData(this.cityName).subscribe(data => {
+      this.weatherData = data;
+      console.log(this.weatherData);
+  });
+  }
+}
