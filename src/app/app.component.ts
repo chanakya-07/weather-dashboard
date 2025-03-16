418a8340043d05ed92cc41b43b3b795fdd0f1e39
NikhilPalliCode <nikhilpalle1997@gmail.com>
2025-03-16 18:48:02 -0500

    fixed https

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DecimalPipe } from '@angular/common';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { WeatherService } from './services/weather.service';
-import { WeatherData } from './models/weather.models';
 import { FormsModule } from '@angular/forms';
-
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FormsModule, DecimalPipe],
+  imports: [RouterOutlet, FormsModule, DecimalPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  weatherData!:any;
-  cityName: string = "Timisoara";
-
-  url = `https://api.weatherapi.com/v1/current.json`;
-  key = 'ebd317e8cbb14aa6ab6204524251801';
-  
+  weatherData!: any;
+  cityName: string = 'Timisoara';
 
   constructor(private weatherService: WeatherService) {}
 
-
   ngOnInit(): void {
     this.getWeatherData(this.cityName);
-    this.cityName = "";
-}
+    this.cityName = '';
+  }
 
-  onSubmit(){
+  onSubmit() {
     this.getWeatherData(this.cityName);
-    this.cityName = "";
+    this.cityName = '';
   }
 
   private getWeatherData(cityName: string) {
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,17 +1,15 @@
-import { Injectable, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
-import { WeatherData } from '../models/weather.models';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
-  private apiKey = 'ebd317e8cbb14aa6ab6204524251801'; 
-  private baseUrl = 'https://api.weatherapi.com/v1';
+  private apiKey = 'ebd317e8cbb14aa6ab6204524251801';
+  private baseUrl = 'https://api.weatherapi.com/v1'; // Use HTTPS here
 
   constructor(private http: HttpClient) {}
-
 
   getWeatherData(city: string): Observable<any> {
     const url = `${this.baseUrl}/current.json?key=${this.apiKey}&q=${city}`; // Use HTTPS here
